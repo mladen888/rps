c67ff7aa5eaaea10578a01d0e855590450177589
Mladen <ikicmladenn@gmail.com>
2024-02-22 18:15:20 +0100

    predposlednji korak

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 let rps;
+let b;
 
 function getComputerChoice() {
-    let a = Math.floor(Math.random() * 3) + 1;
-    if (a === 1) {
-        rps = 'Rock';
+    let a = Math.floor(Math.random() * 3);
+    if (a === 0) {
+        return 'rock';
+    } else if (a === 1) {
+        return 'paper';
     } else if (a === 2) {
-        rps = 'Paper';
-    } else if (a === 3) {
-        rps = 'Scissors';
+        return 'scissors';
     }
-    
-    return rps;
+}
+
+function getPlayersChoice() {
+    let input = prompt("Unesi Rock, Paper, ili Scissors");
+    while (input === null || !['rock', 'paper', 'scissors'].includes(input.toLowerCase())) {
+        input = prompt("Unesi: Rock, Paper, ili Scissors");
+    }
+    return input.toLowerCase();
 }
 
 const computerSelection = getComputerChoice();
+const playersChoice = getPlayersChoice();
+let winCount =0
+let brojPartija =0
+function playRound(computerSelection, playersChoice, winCount) {
+   
+        if (computerSelection === playersChoice) {
+            brojPartija ++;
+            return 'Nereseno';
+        } else if (
+            (computerSelection === 'rock' && playersChoice === 'scissors') ||
+            (computerSelection === 'paper' && playersChoice === 'rock') ||
+            (computerSelection === 'scissors' && playersChoice === 'paper')
+        ) {
+            brojPartija ++;
+            return 'Izgubio si';
+        } else {
+            brojPartija ++;
+            winCount++;
+            return 'Pobedio si';
+        }
+    }
+    
 
-const input = prompt()
+
+console.log("Komp:", computerSelection);
+console.log("Plejer:", playersChoice);
+console.log(playRound(computerSelection, playersChoice));
+
